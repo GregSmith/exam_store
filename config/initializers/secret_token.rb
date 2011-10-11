9b6aca6b53938a202855ab2e8c84367e07b13e72
Greg Smith <greg.smith@alpha-score.com>
2011-10-11 14:41:31 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ExamStore::Application.config.secret_token = '0ed3ef873d5089013e4d9f09b078d52bc21b9c67d994d31eb582850b80944886565255b97f3de6f6e75e103d1f3d67330149b29390f765c2c59a4319c8b9003b'
